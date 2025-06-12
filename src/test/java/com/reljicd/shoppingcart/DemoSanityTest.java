caa5b214c7759dbf37407fd45060ac16d626497c
Brian Bills <brianbills21@usreliance.com>
2025-06-12 02:05:06 -0700

    Created DemoSanityTest.java

---
+++ src/test/java/com/reljicd/shoppingcart/DemoSanityTest.java
@@ -0,0 +1,13 @@
+package com.reljicd.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoSanityTest {
+
+    @Test
+    void itShouldPass() {
+        assertTrue(1 + 1 == 2);
+    }
+}
+
